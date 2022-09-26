3f6128a396467cc508da747dbe9d9f27e7da2c59
xaviguasch <xaviguasch@gmail.com>
2022-09-26 11:31:31 +0200

    combined pre-fetching with client-side rendering

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
 import useSWR from 'swr'
 
-function LastSalesPage() {
-  const [sales, setSales] = useState()
+function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales)
   // const [isLoading, setIsLoading] = useState(false);
 
   const fetcher = (url) => fetch(url).then((res) => res.json())
@@ -51,7 +51,7 @@ function LastSalesPage() {
     return <p>Failed to load.</p>
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>
   }
 
@@ -64,6 +64,25 @@ function LastSalesPage() {
       ))}
     </ul>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    'https://next-js-course-xavi-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
+  )
+  const data = await response.json()
+
+  const transformedSales = []
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    })
+  }
+
+  return { props: { sales: transformedSales }, revalidate: 10 }
 }
 
 export default LastSalesPage
